5fe3c311f987e251a80c9a01293367ba0c6cb87d
Shivoy Arora <shivoy1183@gmail.com>
2024-11-10 19:26:15 +0530

    remote control robot debuged

--- api.py
+++ api.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
-# from gpiozero import Robot, LineSensor, Motor
+from gpiozero import Robot, LineSensor, Motor
 from signal import pause
 from time import sleep
+from math import cos, degrees, pow, sqrt
 
+###############################################################################
+# Showing colors in print
+###############################################################################
+class Colors:
+    BOLD = "\033[1m"
+    BLACK = "\033[0;30m"
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    YELLOW = "\033[0;33m"
+    BLUE = "\033[0;34m"
+    PURPLE = "\033[0;35m"
+    CYAN = "\033[0;36m"
+    WHITE = "\033[0;37m"
+    RESET = "\033[0m"
 
 ###############################################################################
 # Global Vars
@@ -32,16 +47,16 @@ S5_PIN = 23
 ###############################################################################
 # Initialize the Robot
 ###############################################################################
-# S1 = LineSensor(S1_PIN)
-# S2 = LineSensor(S2_PIN)
-# S3 = LineSensor(S3_PIN)
-# S4 = LineSensor(S4_PIN)
-# S5 = LineSensor(S5_PIN)
-#
-# leftMotor = Motor(LEFT_FORWARD_PIN, LEFT_BACKWARD_PIN, enable=LEFT_ENABLE_PIN, pwm=True)
-# rightMotor = Motor(RIGHT_FORWARD_PIN, RIGHT_BACKWARD_PIN, enable=RIGHT_ENABLE_PIN, pwm=True)
-#
-# robot = Robot(leftMotor, rightMotor)
+S1 = LineSensor(S1_PIN)
+S2 = LineSensor(S2_PIN)
+S3 = LineSensor(S3_PIN)
+S4 = LineSensor(S4_PIN)
+S5 = LineSensor(S5_PIN)
+
+leftMotor = Motor(LEFT_FORWARD_PIN, LEFT_BACKWARD_PIN, enable=LEFT_ENABLE_PIN, pwm=True)
+rightMotor = Motor(RIGHT_FORWARD_PIN, RIGHT_BACKWARD_PIN, enable=RIGHT_ENABLE_PIN, pwm=True)
+
+robot = Robot(leftMotor, rightMotor)
 
 
 ###############################################################################
@@ -52,10 +67,29 @@ async def move(x: float, y: float, angle: float):
     x = x / 100
     y = y / 100
 
-    # Get curve radius based on angle (in radians)
-    
+    # Calculating the speed
+    speed = max(min(sqrt(pow(x, 2) + pow(y, 2)), 1), 0)
 
-    print(x, y, angle)
+    print(Colors.GREEN + Colors.BOLD + f"x: {x}, y: {y}, speed: {speed}" + Colors.RESET)
+
+    # Forward or backward
+    if y > 0:
+        if x < 0:
+            robot.forward(speed=speed, curve_left=abs(x))
+        elif x > 0:
+            robot.forward(speed=speed, curve_right=abs(x))
+        else:
+            robot.forward(speed=speed)
+    elif y < 0:
+        if x < 0:
+            robot.backward(speed=speed, curve_left=abs(x))
+        elif x > 0:
+            robot.backward(speed=speed, curve_right=abs(x))
+        else:
+            robot.backward(speed=speed)
+    else:
+        robot.stop()
+    
     return {"x": x, "y": y}
 
 # ###############################################################################
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let main_url = "http://127.0.0.1:8000";
+let main_url = "http://raspberrypi.local:8000";
 var canvas, ctx;
 
 window.addEventListener('load', () => {
